8f90b20a313102d7958613dc4419a0cd787d289e
tcyeee <tcyeee@outlook.com>
2025-10-21 21:44:21 +0800

    add listener for  Alert & Message info

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { notify } from './utils/notify'
 import { alert } from './utils/alert'
 import router from './router'
 import './assets/global.css';
+import { setupTauriListener } from './plugins/tauriListener'
+
+// 添加Alert & Message 信号监听
+setupTauriListener()
 
 const app = createApp(App);
 const pinia = createPinia();
